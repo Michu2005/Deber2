73bc328b0e3d396bb30405d156eb5589f3c95b93
Michelle Vaca <michu.vb2005@gmail.com>
2023-07-15 23:19:46 -0500

    Modificacion nombre en pantalla

--- Deber2Michelle/Deber2Michelle/Program.cs
+++ Deber2Michelle/Deber2Michelle/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Deber2Michelle;
 
+Console.WriteLine("-------- DEBER 2 MICHELLE VACA --------");
+// Instanciar clases Vehiculo, Automovil y Camioneta
 Vehiculo v1 = new Vehiculo("Chevrolet", "Onix", 2021);
 Automovil a1 = new Automovil("VW","Polo", 2019, 5);
 Camioneta c1 = new Camioneta("Mazda", "BT50", 2020, 1.7f);
+Console.WriteLine();
 
 Console.WriteLine("-------- VEHICULO --------");
 Console.WriteLine(v1.getInformation());
